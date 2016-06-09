a2c30e8e9e2750e57a96d07614053ba403ae7e9f
davidpasek <david.pasek@gmail.com>
2016-06-09 09:56:39 +0200

    Clone from template

--- create-vms.ps1
+++ create-vms.ps1
@@ -1,4 +1,6 @@
-﻿# PowerCLI script to create 5 VMs
+﻿# PowerCLI script to create Virtual Machines
+# ===========================================
+# It can create new VM or deploy VM from template if template name is specified.
 #
 #————————————————
 # Start of script parameters section
@@ -9,25 +11,10 @@ $vcenteruser = “readwrite“
 $vcenterpw = “readwrite“
 #
 # Specify number of VMs you want to create
-$vm_count = “50“
-#
-# Specify number of VM CPUs
-$numcpu = “1“
-#
-# Specify number of VM MB RAM
-$MBram = “512“
-#
-# Specify VM disk size (in MB)
-$MBguestdisk = “2000“
-#
-# Specify VM disk type, available options are Thin, Thick, EagerZeroedThick
-$Typeguestdisk =”Thick“
-#
-# Specify VM guest OS
-$guestOS = “winNetStandardGuest“
+$vm_count = “5“
 #
 # Specify vCenter Server datastore
-$ds = “TEST“
+$Datastore = “TEST“
 #
 # Specify vCenter Server Virtual Machine & Templates folder
 $Folder = “TEST”
@@ -36,13 +23,34 @@ $Folder = “TEST”
 $Cluster = “TEST“
 #
 # Specify the VM name to the left of the – sign
-$VM_prefix = “TEST-“
+$VM_prefix = “XTEST-“
 #
 # Specify the VM provisioning type (sync/async) - true = async (parallel), false = sync (sequentional)
-$VM_create_async = $true
+$VM_create_async = $false
+#
+# Specify if VM should be created from template
+$VM_from_template="TEST_template"
 #
 # Specify if VM should be Power On 
 $VM_power_on = $false
+#
+# Parameters below are used only for new VM. 
+# VM created from template has these parameters included in the template.
+#
+# Specify number of VM CPUs
+$numcpu = “1“
+#
+# Specify number of VM MB RAM
+$MBram = “512“
+#
+# Specify VM disk size (in MB)
+$MBguestdisk = “1000“
+#
+# Specify VM disk type, available options are Thin, Thick, EagerZeroedThick
+$Typeguestdisk =”Thick“
+#
+# Specify VM guest OS
+$guestOS = “winNetStandardGuest“
 #
 # End of script parameter section
 #—————————————— #
@@ -57,13 +65,20 @@ write-host “Connecting to vCenter Server $vcenter” -foreground green
 $vc = connect-viserver $vcenter -User $vcenteruser -Password $vcenterpw
 #
 
+#$O_cluster=Get-Cluster $Cluster
+
 1..$vm_count | foreach {
   $VM_postfix=”{0:D2}” -f $_
   $VM_name= $VM_prefix + $VM_postfix
-  $ESXi=Get-Cluster $Cluster | Get-VMHost -state connected | Get-Random
+  #$O_ESXi=Get-Cluster $Cluster_name | Get-VMHost -state connected | Get-Random
 
-  write-host “Creation of VM $VM_name initiated”  -foreground green
-  New-VM -RunAsync:$VM_create_async -Name $VM_Name -VMHost $ESXi -numcpu $numcpu -MemoryMB $MBram -DiskMB $MBguestdisk -DiskStorageFormat $Typeguestdisk -Datastore $ds -GuestId $guestOS -Location $Folder
+  if ($VM_from_template -eq "") {
+    write-host “Creation of VM $VM_name initiated”  -foreground green
+    New-VM -RunAsync:$VM_create_async -Name $VM_Name -ResourcePool $Cluster -numcpu $numcpu -MemoryMB $MBram -DiskMB $MBguestdisk -DiskStorageFormat $Typeguestdisk -Datastore $Datastore -GuestId $guestOS -Location $Folder
+  } else {
+    write-host “Deployment of VM $VM_name from template $VM_from_template initiated”  -foreground green
+    New-VM -RunAsync:$VM_create_async -Name $VM_Name -Template $VM_from_template -ResourcePool $Cluster -Datastore $Datastore -Location $Folder
+  }
 
   if ($VM_power_on) {
     write-host “Power On of the  VM $VM_name initiated" -foreground green
