4b48e6272cf2353f0901f34e26e964129d82e7b7
David Pasek <david.pasek@gmail.com>
2025-07-03 21:54:24 +0200

    Create list-vms-with-custom-attribute.ps1

---
+++ list-vms-with-custom-attribute.ps1
@@ -0,0 +1,2 @@
+#  Onliner to list VMs with custom attribute "Last Backup"
+Get-VM | Select-Object Name, @{N='LastBackup';E={($_.CustomFields | Where-Object {$_.Key -match "Last Backup"}).Value}} | Where-Object {$_.LastBackup -ne $null -and $_.LastBackup -ne ""}
