21994b21d7c4be46016d619f2fa6855c7d3eeb14
David Pasek <david.pasek@gmail.com>
2025-07-17 13:10:53 +0200

    Update connect-to-vcenter.ps1

--- connect-to-vcenter.ps1
+++ connect-to-vcenter.ps1
@@ -3,4 +3,5 @@ Write-Host "Connecting to vCenter ..."
 $VC = Read-Host "Enter one vCentre Server or multiple vCenter servers delimted by comma."
 Write-Host "Enter vCenter credentials ..."
 $CRED = Get-Credential
-Connect-VIServer -Server $VC -Credential $CRED -ErrorAction Stop | Out-Null
+#Connect-VIServer -Server $VC -Credential $CRED -ErrorAction Stop | Out-Null
+Connect-VIServer -Server $VC -Credential $CRED
