ec12214a0f4fdaa94c49dee42433f7c7ef1ad05b
David Pasek <david.pasek@gmail.com>
2025-07-17 12:13:57 +0200

    Create connect-to-vcenter.ps1

---
+++ connect-to-vcenter.ps1
@@ -0,0 +1,6 @@
+# Connect to vCenter
+Write-Host "Connecting to vCenter ..."
+$VC = Read-Host "Enter one vCentre Server or multiple vCenter servers delimted by comma."
+Write-Host "Enter vCenter credentials ..."
+$CRED = Get-Credential
+Connect-VIServer -Server $VC -Credential $CRED -ErrorAction Stop | Out-Null
